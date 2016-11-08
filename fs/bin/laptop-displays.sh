f91ffe19f4234bd736782e887ead1a29e81aa730
Nikola Kocic <nikolakocic@gmail.com>
2016-11-08 13:51:50 +0100

    Add script for laptop display management

---
+++ fs/bin/laptop-displays.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
+export XAUTHORITY=/home/kocic/.Xauthority
+export DISPLAY=:0
+
+args=()
+
+function turn_on_docked_displays()
+{
+    args+=(--output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal)
+    args+=(--output DP2 --mode 1920x1080 --pos 0x0 --rotate normal)
+}
+
+function turn_off_docked_displays()
+{
+    args+=(--output DP1 --off --output DP2 --off)
+}
+
+function turn_on_laptop_display()
+{
+    args+=(--output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal)
+}
+
+function turn_off_laptop_display()
+{
+    args+=(--output eDP1 --off)
+}
+
+DOCK_MONITOR_STATUS=$(cat /sys/class/drm/card0-DP-1/status)  # connected or disconnected
+echo "DOCK_MONITOR_STATUS=${DOCK_MONITOR_STATUS}"
+
+if [ "${DOCK_MONITOR_STATUS}" == "connected" ] ; then
+  turn_on_docked_displays
+elif [ "${DOCK_MONITOR_STATUS}" == "disconnected" ] ; then
+  turn_off_docked_displays
+else
+  exit
+fi
+
+LID_STATUS=$(cat /proc/acpi/button/lid/LID0/state | cut -d' ' -f2- | sed 's/^ *//')  # open or closed
+echo "LID_STATUS=${LID_STATUS}"
+
+if [ "${LID_STATUS}" == "open" ] ; then
+  turn_on_laptop_display
+elif [ "${LID_STATUS}" == "closed" ] ; then
+  turn_off_laptop_display
+else
+  exit
+fi
+
+args+=(--output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off  --output VIRTUAL1 --off)
+
+xrandr "${args[@]}"
