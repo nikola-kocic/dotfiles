2f8d75ef46bfc09b4967b4e67715325c390fd6cb
Nikola Kocic <nikolakocic@gmail.com>
2022-08-07 14:00:21 +0200

    set_urgent_hint.sh: ignore chromium

--- fs/bin/set_urgent_hint.sh
+++ fs/bin/set_urgent_hint.sh
@@ -4,4 +4,8 @@ if [[ $1 == "Konsole" ]]; then
     # Konsole itself sets urgent hint to a proper window
     exit 0
 fi
+if [[ $1 == "Chromium" ]]; then
+    # Chromium itself sets urgent hint to a proper window via extension https://github.com/nikola-kocic/attention-on-notification-chrome
+    exit 0
+fi
 wmctrl -r $1 -b add,demands_attention
